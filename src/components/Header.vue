730302e189880046658777647ebf972741750666
Forklein <giuseppepisani2@gmail.com>
2021-09-09 13:42:19 +0200

    Set all files with components and style. Set Header

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="row py-3">
+        <div class="col-6">
+          <img src="@/assets/img/logo.svg" alt="logo" style="max-width: 50%" />
+        </div>
+        <div class="col-6 d-flex justify-content-center align-items-center">
+          <select>
+            <option selected disabled>Seleziona un Genere</option>
+            <option>Test</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/_variables.scss";
+
+header {
+  background-color: $header-color;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">Test</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Main",
+  data() {
+    return {
+      cards: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        console.log(res.data.response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
